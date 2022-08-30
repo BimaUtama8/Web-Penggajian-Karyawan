db4358c20aac995c193cc92e0da8f1cdd272e941
Bima Satria Utama <72238731+BimaUtama8@users.noreply.github.com>
2022-08-30 20:03:11 +0700

    initial commit

---
+++ resources/views/login/login.blade.php
@@ -0,0 +1,125 @@
+@extends('layouts.login')
+
+@section('content')
+<div class="auth-page-wrapper auth-bg-cover py-5 d-flex justify-content-center align-items-center min-vh-100">
+    <div class="bg-overlay">
+        <img src="{{ asset('login/img/bg-smart-home-1.jpg') }}" style="width: 100%clear">
+    </div>
+    <!-- auth-page content -->
+    <div class="auth-page-content overflow-hidden pt-lg-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card overflow-hidden">
+                        <div class="row g-0">
+                            <div class="col-lg-6">
+                                <div class="p-lg-5 p-4 auth-one-bg h-100">
+                                    <div class="bg-overlay"></div>
+                                    <div class="position-relative h-100 d-flex flex-column">
+                                        <div class="mb-4">
+                                            <a href="index.html" class="d-block">
+                                                <img src="assets/images/logo-light.png" alt="" height="18">
+                                            </a>
+                                        </div>
+                                        <div class="mt-auto">
+                                            <div class="mb-3">
+                                                <i class="ri-double-quotes-l display-4 text-success"></i>
+                                            </div>
+
+                                            <div id="qoutescarouselIndicators" class="carousel slide" 
+                                                data-bs-ride="carousel">
+                                                <div class="carousel-indicators">
+                                                    <button type="button" data-bs-target="#qoutescarouselIndicators"
+                                                        data-bs-slide-to="0" class="active" aria-current="true"
+                                                        aria-label="Slide 1"></button>
+                                                    <button type="button" data-bs-target="#qoutescarouselIndicators"
+                                                        data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                                    <button type="button" data-bs-target="#qoutescarouselIndicators"
+                                                        data-bs-slide-to="2" aria-label="Slide 3"></button>
+                                                </div>
+                                                <div class="carousel-inner text-center text-white-50 pb-5">
+                                                    <div class="carousel-item active">
+                                                        <p class="fs-15 fst-italic">" Great! Clean code, clean design,
+                                                            easy for customization. Thanks very much! "</p>
+                                                    </div>
+                                                    <div class="carousel-item">
+                                                        <p class="fs-15 fst-italic">" The theme is really great with an
+                                                            amazing customer support."</p>
+                                                    </div>
+                                                    <div class="carousel-item">
+                                                        <p class="fs-15 fst-italic">" Great! Clean code, clean design,
+                                                            easy for customization. Thanks very much! "</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <!-- end carousel -->
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- end col -->
+
+                            <div class="col-lg-6">
+                                <div class="p-lg-5 p-4">
+                                    <div>
+                                        <h5 class="text-primary">Welcome Back !</h5>
+                                        <p class="text-muted">Sign in</p>
+                                    </div>
+                                    @foreach($errors->all() as $error)
+                                        <div class="alert alert-danger fade show">
+                                            <strong>{{ $error }}</strong>
+                                        </div>
+                                    @endforeach
+                                    <div class="mt-4">
+                                        <form method="POST" action="{{ route('auth_login') }}">
+                                            @csrf
+                                            <div class="mb-3">
+                                                <label for="email" class="form-label">Username</label>
+                                                <input type="text" class="form-control" id="email" name="email"
+                                                    placeholder="Enter username">
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="password" class="form-label">Password</label>
+                                                <input type="password" class="form-control" id="password" name="password"
+                                                    placeholder="Enter password">
+                                            </div>  
+
+                                            <div class="mt-4">
+                                                <button class="btn btn-success w-100" type="submit">Sign In</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- end col -->
+                        </div>
+                        <!-- end row -->
+                    </div>
+                    <!-- end card -->
+                </div>
+                <!-- end col -->
+
+            </div>
+            <!-- end row -->
+        </div>
+        <!-- end container -->
+    </div>
+    <!-- end auth page content -->
+
+    <!-- footer -->
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="text-center">
+                        <script>
+                            document.write(new Date().getFullYear())
+                        </script> PT. SURYA GLOBALINDO SEJAHTERA
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- end Footer -->
+</div>
+@endsection
