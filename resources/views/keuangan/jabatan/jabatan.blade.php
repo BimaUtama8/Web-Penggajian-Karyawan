6574e230720452cff744f199ac433bfc48536e9b
Bima Satria Utama <72238731+BimaUtama8@users.noreply.github.com>
2022-12-15 23:22:33 +0700

    sweet alert

--- resources/views/keuangan/jabatan/jabatan.blade.php
+++ resources/views/keuangan/jabatan/jabatan.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 @section('content')
+@if (session('success_message'))
+    <div class="alert alert-success">
+        {{ session('success_message') }}
+    </div>
+@endif
 <div class="row">
     <div class="col-12">
         <div class="page-title-box d-sm-flex align-items-center justify-content-between">
@@ -59,22 +64,13 @@
                     @endforeach
                 </tbody>
             </table>
-            <div class="noresult" style="display: none">
-                <div class="text-center">
-                    <lord-icon src="https://cdn.lordicon.com/msoeawqm.json" trigger="loop"
-                        colors="primary:#25a0e2,secondary:#00bd9d" style="width:75px;height:75px">
-                    </lord-icon>
-                    <h5 class="mt-2">Sorry! No Result Found</h5>
-                    <p class="text-muted mb-0">We've searched more than 150+ Orders We did not find any
-                        orders for you search.</p>
-                </div>
-            </div>
         </div>
     </div>
 </div>
 </div>
 </div>
 </div>
+@include('sweetalert::alert')
 <script>
     $(document).ready(function () {
         $('#myTable').DataTable();
